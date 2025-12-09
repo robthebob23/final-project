9b17bafd59953e71159aede52253005bbbb75efb
robthebob23 <113632761+robthebob23@users.noreply.github.com>
2025-12-08 23:01:52 -0500

    Add files via upload

---
+++ proposal.py
@@ -0,0 +1,33 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+Arc Raiders Arc defense
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+My project is a game that allows you to play through waves of enenmies gaining experience and loot the the more you win. 
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+It will be able to track wins, have you perform different actions against arcs, find loot to help you as you go and level your character up 
+TODO
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+no other course
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+TODO, if applicable
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+I want the game to be playable no matter where i can do action to destroy the enemy which in this case is arcs
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+I want to be able to add various feature to the game such as xp loot and different wepons.
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+Best out come is if I have all those feature plus boss fights and arcs that increase in difficulty the longer you go
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+I will need to figure how to make the game work and also how to format everything so the game looks how it should. 
