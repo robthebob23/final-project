6ced24b05003c68d92b8c4e6bf94d4ec68cd22ba
robthebob23 <113632761+robthebob23@users.noreply.github.com>
2025-12-08 23:24:24 -0500

    Add files via upload

---
+++ status.py
@@ -0,0 +1,23 @@
+# Status Report
+
+#### Your name
+Roberto Quiroz
+
+#### Your section leader's name
+Negar Koushki
+
+#### Project title
+Negar Koushki
+
+***
+
+Short answers for the below questions suffice. If you want to alter your plan for your project (and obtain approval for the same), be sure to email your section leader directly!
+
+#### What have you done for your project so far?
+Made parts of the base game still figuring out how to put it all together so it can work
+
+#### What have you not done for your project yet?
+All the extras to the game like extra features to make it more fun
+
+#### What problems, if any, have you encountered?
+Some of the math for the game and working out some of the loops was difficult also figuring out how to format the print statements so everything would look good was a big challenge.
